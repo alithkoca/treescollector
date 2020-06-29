465bd2f4280e97c214e34c10c2ad2edf3f1b0832
alithkoca <34367042+alithkoca@users.noreply.github.com>
2020-06-29 21:29:13 +0300

    Add files via upload

---
+++ heroku-master/dist/map_handler.js
@@ -0,0 +1,120 @@
+var baseMapLayer = new ol.layer.Tile({
+    source: new ol.source.OSM()
+});
+
+var layer = new ol.layer.Tile({
+    source: new ol.source.OSM()
+});
+
+var center = ol.proj.fromLonLat([32, 39]);
+
+var view = new ol.View({
+    center: center,
+    zoom: 10
+});
+
+var map = new ol.Map({
+    target: 'map',
+    view: view,
+    layers: [layer]
+});
+
+var vectorSource = new ol.source.Vector({
+    url:"/api/data",
+    format: new ol.format.GeoJSON({ featureProjection: "EPSG:4326" })
+});
+
+var vectorSource2 = new ol.source.Vector({
+    url:"/api/data2",
+    format: new ol.format.GeoJSON({ featureProjection: "EPSG:4326" })
+});
+
+var vectorSource3 = new ol.source.Vector({
+    url:"/api/data3",
+    format: new ol.format.GeoJSON({ featureProjection: "EPSG:4326" })
+});
+
+var markerVectorLayer = new ol.layer.Vector({
+    source: vectorSource,
+    style: new ol.style.Style({
+        image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+            anchor: [0.5, 15],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            opacity: 0.75,
+            src: 'https://img.icons8.com/plasticine/100/000000/coniferous-tree.png'
+    }))
+    })
+});
+
+var markerVectorLayer2 = new ol.layer.Vector({
+    source: vectorSource2,
+    style: new ol.style.Style({
+        image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+            anchor: [0.5, 15],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            opacity: 0.75,
+            src: 'https://img.icons8.com/flat_round/64/000000/tree.png'
+    }))
+    })
+});
+
+var markerVectorLayer3 = new ol.layer.Vector({
+    source: vectorSource3,
+    style: new ol.style.Style({
+        image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+            anchor: [0.5, 15],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            opacity: 0.75,
+            src: 'https://img.icons8.com/emoji/48/000000/palm-tree-emoji.png'
+    }))
+    })
+});
+
+map.addLayer(markerVectorLayer);
+map.addLayer(markerVectorLayer2);
+map.addLayer(markerVectorLayer3);
+var select = new ol.interaction.Select({multiple:false});
+select.on('select', fnHandler);
+map.addInteraction(select);
+map.on("click",handleMapClick);
+
+function handleMapClick(evt) {
+    var coord=ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+    document.getElementById("Latitude").value=coord[1];
+    document.getElementById("Longitude").value=coord[0];
+}
+
+function fnHandler(e) {
+    var coord = e.mapBrowserEvent.coordinate;
+    let features = e.target.getFeatures();
+    features.forEach( (feature) => {
+        console.log("Tree Type: " + feature.getProperties().tree_type);
+        console.log("Tree Height: " + feature.getProperties().height);
+        document.getElementById("tree_type").value=feature.getProperties().tree_type;
+    });
+    
+    if (e.selected[0]) {
+        var coords=ol.proj.transform(e.selected[0].getGeometry().getCoordinates(), 'EPSG:3857', 'EPSG:4326');
+        document.getElementById("Latitude").value=coords[1];
+        document.getElementById("Longitude").value=coords[0];
+        console.log(coords);
+ }
+}
+
+function submit() 
+{ 
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/post", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    var data=JSON.stringify({
+ 
+        Latitude: document.getElementById('Latitude').value,
+        Longitude: document.getElementById('Longitude').value,
+        tree_type: document.getElementById('tree_type').value,
+        Height: document.getElementById('Height').value
+    });
+    xhr.send(data);
+}
---
+++ heroku-master/server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const pg=require("pg").Pool;
+const bodyParser = require('body-parser');
+const app = express();
+
+
+const pool=new pg({host:'ec2-54-75-246-118.eu-west-1.compute.amazonaws.com',
+                database:'df8pmnnvo07v5p',
+                user:'dacgmwxdvgdcjl',
+                password:'00baee341dd86a80a6428bd20c118bacb7edef98d17b900f6d8078e70536a441',
+                port:5432,
+                ssl: { rejectUnauthorized: false }
+            });
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const _port = process.env.PORT || 5000;
+const _app_folder = __dirname + '/dist' ;
+
+app.use(express.static(__dirname + '/dist' ));
+
+var table_name="tree2";
+
+app.get("/api/data",function(req,res)
+{
+    pool.query("SELECT jsonb_build_object('type','FeatureCollection','features', jsonb_agg(feature)) FROM (SELECT jsonb_build_object('type','Feature','geometry',ST_AsGeoJSON(geom)::jsonb, 'properties', to_jsonb(row) - 'gid' - 'geom') AS feature FROM (SELECT * FROM "+table_name+" WHERE tree_type='Çam') row) features;", (err1, res1) => {
+
+        if(err1) {return console.log(err1);}
+        res.json(res1.rows[0]["jsonb_build_object"]);
+    });
+});
+
+app.get("/api/data2",function(req,res)
+{
+    pool.query("SELECT jsonb_build_object('type','FeatureCollection','features', jsonb_agg(feature)) FROM (SELECT jsonb_build_object('type','Feature','geometry',ST_AsGeoJSON(geom)::jsonb, 'properties', to_jsonb(row) - 'gid' - 'geom') AS feature FROM (SELECT * FROM "+table_name+" WHERE tree_type='Kavak') row) features;", (err1, res1) => {
+
+        if(err1) {return console.log(err1);}
+        res.json(res1.rows[0]["jsonb_build_object"]);
+    });
+});
+
+app.get("/api/data3",function(req,res)
+{
+    pool.query("SELECT jsonb_build_object('type','FeatureCollection','features', jsonb_agg(feature)) FROM (SELECT jsonb_build_object('type','Feature','geometry',ST_AsGeoJSON(geom)::jsonb, 'properties', to_jsonb(row) - 'gid' - 'geom') AS feature FROM (SELECT * FROM "+table_name+" WHERE tree_type='Palmiye') row) features;", (err1, res1) => {
+
+        if(err1) {return console.log(err1);}
+        res.json(res1.rows[0]["jsonb_build_object"]);
+    });
+});
+app.post('/post', function(request, response){
+    pool.query("INSERT INTO "+table_name+" VALUES('"+request.body.tree_type+"',ST_SETSRID(ST_MAKEPOINT("+request.body.Longitude+","+request.body.Latitude+"),4326),'"+request.body.Height+"');", (err1, res1) =>
+    {
+        if(err1)
+            { console.log(request.body);
+            return console.log(err1);}
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/plain');
+        response.end('Data Store Success!\n');
+    });
+});
+
+app.all('*', function (req, res) {
+    res.status(200).sendFile(`/`, {root: _app_folder});
+});
+
+   app.listen(_port, function () {
+    console.log("Node Express server for " + app.name + " listening on http://localhost:" + _port);
+});
+   
